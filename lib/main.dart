cac2fea9a46dabdb90de3e155016c79c7e64f674
Akash Belekar <112610706+AkashBelekar2428@users.noreply.github.com>
2024-01-10 10:10:15 +0530

    Add Time_Calender library

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
-import 'package:cr_calendar/cr_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main(){
   runApp(MyDeailyPlannerApp());
@@ -26,22 +26,101 @@ class MyDeailyPlannerScreen extends StatefulWidget {
   State<MyDeailyPlannerScreen> createState() => MyDailyPlannerstate();
 }
   class MyDailyPlannerstate extends State<MyDeailyPlannerScreen>{
-  CrCalendarController _controller = CrCalendarController();
+
+    CalendarFormat _calendarFormat = CalendarFormat.month;
+    DateTime _focusedDay = DateTime.now();
+    DateTime _selectedDay = DateTime.now();
+    Map<DateTime, List<dynamic>> _events = {};
+
+    TextEditingController _eventController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
    return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body:  CrCalendar(controller: _controller,
-          initialDate: DateTime.now(),
-        backgroundColor: Colors.white,
-      ),
-
-
+      body: Column(
+        children: [
+          TableCalendar(focusedDay:_focusedDay ,
+              firstDay: DateTime.now(),
+              lastDay: DateTime.utc(2030, 12, 31),
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format){
+            setState(() {
+              _calendarFormat = format;
+            });
+            },
+            onDaySelected: (selected, focusDay){
+            setState(() {
+              _selectedDay = selected;
+              _focusedDay = focusDay;
+            });
+            },
+            calendarStyle: CalendarStyle(
+              todayTextStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+              )
+            ),
+            selectedDayPredicate: (day){
+            return isSameDay (_selectedDay,day);
+            },
+            eventLoader: (day){
+            return _events[day] ?? [];
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+              child:SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ...?_events[_selectedDay]?.map((event) => ListTile(
+                      title: Text(event),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete), onPressed: () {
+                          setState(() {
+                            _events[_selectedDay]?.remove(event);
+                          });
+                      },
+                      ),
+                    )) ?? [],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Add Event:'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _eventController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Event',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                        onPressed: (){
+                          setState(() {
+                            final event = _eventController.text.toString();
+                            if (event.isNotEmpty){
+                              _events[_selectedDay] ??= [];
+                              _events[_selectedDay]!.add(event);
+                              _eventController.clear();
+                            }
+                          });
+                        },
+                        child: Text('Add Event'))
+                  ],
+                ),
+              )
+          )
+        ],
+      )
       );
-
+   }
   }
-
-}
 
